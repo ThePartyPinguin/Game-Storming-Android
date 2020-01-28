8e78f455887c201eb3a50317c656c811bbd6b949
ThePartyPinguin <joriseijmberts@hotmail.nl>
2020-01-28 13:54:07 +0100

    Added check to see if idea has already been send by a client

--- Assets/Scripts/UiController/SendIdeaMenuUiController.cs
+++ Assets/Scripts/UiController/SendIdeaMenuUiController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using Boo.Lang;
 using GameFrame.UnityHelpers.Networking;
 using GameFrame.UnityHelpers.Networking.Client;
 using TMPro;
@@ -9,6 +10,7 @@ using UnityEngine.UI;
 public class SendIdeaMenuUiController : MonoBehaviour
 {
     public TMP_InputField IdeaInputField;
+    public TMP_Text AlreadySendIdeaWarningLabel;
     public Button SubmitButton;
     public SwipeController SwipeController;
     public float AnimationSpeed;
@@ -17,12 +19,16 @@ public class SendIdeaMenuUiController : MonoBehaviour
     private UnityNetworkManager _networkManager;
     private Animator _animator;
 
+    private List<string> _sendIdeas;
+
     void Start()
     {
         _networkManager = UnityNetworkManager.Instance;
         _buttonText = SubmitButton.GetComponentInChildren<TMP_Text>();
         _animator = GetComponent<Animator>();
+        _sendIdeas = new List<string>();
         SwipeController.OnSwiped += OnSwiped;
+        AlreadySendIdeaWarningLabel.text = string.Empty;
     }
 
     private void OnSwiped()
@@ -35,6 +41,18 @@ public class SendIdeaMenuUiController : MonoBehaviour
     {
         if(IdeaInputField == null || string.IsNullOrWhiteSpace(IdeaInputField.text))
             return;
+
+        if (_sendIdeas.Contains(s => s.Equals(IdeaInputField.text, StringComparison.InvariantCultureIgnoreCase)))
+        {
+            AlreadySendIdeaWarningLabel.text = "You already send this idea, please send a new idea";
+            return;
+        }
+        else
+        {
+            AlreadySendIdeaWarningLabel.text = string.Empty;
+            _sendIdeas.Add(IdeaInputField.text);
+        }
+
         _animator.speed = AnimationSpeed;
         _animator.SetTrigger("SendTrigger");
 
